87088520cc26ee4332db73c325d42ddb2c2cfa65
rastabrandy02 <73582888+rastabrandy02@users.noreply.github.com>
2022-11-10 10:19:35 +0100

    Added Behaviour Trees

--- My project/Assets/Scripts/Chest.cs
+++ My project/Assets/Scripts/Chest.cs
@@ -2,38 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Chest : MonoBehaviour
 {
     public GameObject top;
     public bool hasToOpen;
     public bool isOpen;
+    delegate IEnumerator State();
+    private State state;
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-       
+        state = Idle;
+
+        while (enabled)
+            yield return StartCoroutine(state());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (hasToOpen)
-        {
-           StartCoroutine( Open());
-            hasToOpen = false;
-            
-        }
+        
     }
 
+    IEnumerator Idle()
+    {
+
+        if (hasToOpen)
+        {
+            hasToOpen = false;
+            state = Open;
+            
+
+        }
+        yield return null;
+    }
     IEnumerator Open()
     {
-        for(int i = 0; i <36; i++)
-        {
+       
+        
             
-            top.transform.Rotate(-5.0f, 0.0f, 0.0f);
-            Debug.Log(i);
-            yield return new WaitForSeconds(0.05f);
-            
-        }
-        isOpen = true;
+            for (int i = 0; i < 36; i++)
+            {
+                //if (top.transform.localRotation.eulerAngles.x < -180)
+                //{
+                //break;
+                //}
+
+                top.transform.Rotate(-5.0f, 0.0f, 0.0f);
+                Debug.Log(top.transform.localRotation.eulerAngles.x);
+                //Debug.Log(i);
+
+                yield return new WaitForSeconds(0.05f);
+            }
+           
+            isOpen = true;
+        state = Idle;
+        
+        
     }
 }
