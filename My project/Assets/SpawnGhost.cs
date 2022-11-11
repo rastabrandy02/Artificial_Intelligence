0ffb6f927c8a5634fd812d9404fb577b8e2e3b69
rastabrandy02 <73582888+rastabrandy02@users.noreply.github.com>
2022-11-11 10:22:02 +0100

    Conditions in behaviour tree

---
+++ My project/Assets/SpawnGhost.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Pada1.BBCore;           // Code attributes
+using Pada1.BBCore.Tasks;     // TaskStatus
+using Pada1.BBCore.Framework; // BasePrimitiveAction
+
+[Action("MyActions/SpawnGhost")]
+[Help("Spawns a ghost")]
+public class SpawnGhost : BasePrimitiveAction
+{
+    [InParam("game object")]
+    
+    public GameObject ghost;
+
+    public override TaskStatus OnUpdate()
+    {
+        
+        GameObject.Instantiate(ghost);
+        return TaskStatus.COMPLETED;
+    }
+}
---
+++ My project/Assets/isKnightNear.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+using Pada1.BBCore;
+using Pada1.BBCore.Framework;
+
+[Condition("MyConditions/Is Knight Near?")]
+[Help("Checks whether Knight is near the Whitch.")]
+public class IsKnightNear : ConditionBase
+{
+    public override bool Check()
+    {
+        GameObject whitch = GameObject.Find("Whitch");
+        GameObject knight = GameObject.Find("Knight");
+        return Vector3.Distance(whitch.transform.position, knight.transform.position) < 10f;
+    }
+}
